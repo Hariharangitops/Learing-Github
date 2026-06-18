467be336e67cde5deaeb3df99539c42042bcebad
Hariharangitops <hariharaneee20@gmail.com>
2026-06-18 19:59:54 +0530

    welcome

--- calc.py
+++ calc.py
@@ -1,16 +1,1 @@
-def add(x, y): return x + y
-def subtract(x, y): return x - y
-def multiply(x, y): return x * y
-def divide(x, y): return x / y if y != 0 else "Error"
-
-def calc():
-    while True:
-        choice = input("Op (1:+/2:-/3:*/4:/:5:Exit): ")
-        if choice == '5': break
-        if choice in ('1', '2', '3', '4'):
-            n1, n2 = float(input("N1: ")), float(input("N2: "))
-            ops = {'1': add, '2': subtract, '3': multiply, '4': divide}
-            print("Result:", ops[choice](n1, n2))
-
-if __name__ == "__main__":
-    calc()
+print (Welcome to python file)
